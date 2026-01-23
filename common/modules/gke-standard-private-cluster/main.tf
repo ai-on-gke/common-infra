a87c0c223a20faf1e7e93765635e882cec0b9d6a
Artur Kamalov <artur.kamalov@akvelon.us>
2026-01-23 14:43:49 +0100

    bump all gke dependency module versions

--- common/modules/gke-standard-private-cluster/main.tf
+++ common/modules/gke-standard-private-cluster/main.tf
@@ -18,7 +18,7 @@ locals {
 
 module "gke" {
   source                               = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  version                              = "33.0.0"
+  version                              = "43.0.0"
   project_id                           = var.project_id
   regional                             = var.cluster_regional
   name                                 = var.cluster_name
